12acaa0ad914daefd02676ed5e6551932f5028f1
Hubert <hubert.grobelny@21w.pl>
2021-08-18 12:56:55 +0200

    Fix axios get

--- src/App.js
+++ src/App.js
@@ -15,38 +15,20 @@ state = {
   pressure: '',
   wind: '',
   erro: '',
-  persons: []
+  weatchers: []
 }
-componentDidMount() {
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=d7b3751077889d54b180636f32444101`)
-  .then(resp => console.log(resp.data))
+
+handleCitySubmit = (e) =>{
+  e.preventDefault()
+  const API =`https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=d7b3751077889d54b180636f32444101`;
+  axios.get(API)
+  .then(res => {
+    const weatcher = res.data;
+    console.log(res.data)
+    this.setState({ weatcher })
+  })
     .catch(err => console.log(err))
 }
- 
-
-
-handleCitySubmit = e =>{
-  e.preventDefault()
-  const API = `api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=d7b3751077889d54b180636f32444101&units=metric`;
-  const APIS = `api.openweathermap.org/data/2.5/weather?q=London&appid=d7b3751077889d54b180636f32444101`;
-  fetch(APIS, {
-    headers : { 
-      'Content-Type': 'application/json',
-     }
-
-  })
-  .then(resp => {
-    if(resp.ok){
-      return resp;
-    }
-    throw Error("Błąd")
-  })
-  .then(resp => resp.json())
-  .then(text => console.log(text))
-  .catch(err => console.log(err))
-
-}
-
 handleInputChange = (e) =>{
   this.setState({
     value: e.target.value
@@ -54,18 +36,12 @@ handleInputChange = (e) =>{
 }
   render(){
   return (
-    <>
     <div className="App">
         <Form value={this.props.value} 
         change={this.handleInputChange} 
         submit={this.handleCitySubmit}
         />
-    </div>
-     
-        <ul>
-          { this.state.persons.map(person => <li>{person.name}</li>)}
-        </ul>
-     </>
+    </div> 
   );
 }
 }
